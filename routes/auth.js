ea44022d23db8dba40abad746f40bb88f67d1a5b
Gustavo Bravo <gandresbravo90@icloud.com>
2025-05-07 21:58:31 +0200

    Reemplazo bcrypt por bcryptjs (sin binarios nativos)

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 // routes/auth.js
 const express  = require('express');
 const router   = express.Router();
-const bcrypt   = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const db       = require('../database');
 const rolePermissions = require('../config/permissions'); // ← Cargamos nuestro mapa de permisos
 
